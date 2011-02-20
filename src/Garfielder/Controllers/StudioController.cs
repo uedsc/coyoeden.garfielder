d07b368b52e8ae3a4f7b2e30ee93095b8271d579
Levin <Levin@localhost>
2011-02-20 21:30:26 +0800

    gggggg

---
+++ src/Garfielder/Controllers/StudioController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using Garfielder.Models;
+using Garfielder.ViewModels;
+
+namespace Garfielder.Controllers
+{
+    public class StudioController : Controller
+    {
+		public ActionResult Index()
+		{
+			var topics = new List<VMTopic>();
+			var topics1 = Topic.ListAllStarred(items => items.ForEach(x => topics.Add(new VMTopic
+			{
+				Id = x.Id,
+				Title = x.Title,
+				Slug = x.Slug,
+				Desc = x.Description,
+				Logo = x.Icon == null ? Url.Content("~/assets/img/default.jpg") : x.Icon.Url(ImageFlags.S160X100),
+				DateCreated = x.CreatedAt
+			})));
+			var vm = new VMHome
+			{
+				TopicNum = topics.Count,
+				Topics = topics
+			};
+
+			//TODO:fake Login
+			fakeLogin();
+			return View(vm);
+		}
+
+		private void fakeLogin()
+		{
+			using (var db = new GarfielderEntities())
+			{
+				var user = db.Users.First();
+				HttpContext.Items["CurrentUser"] = user;
+				FormsAuthentication.SetAuthCookie(user.Name, false);
+			}
+		}
+
+    }
+}
---
+++ src/Garfielder/Views/Studio/Index.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Views/Shared/Site.Master" Inherits="System.Web.Mvc.ViewPage<Garfielder.ViewModels.VMHome>" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="TitleContent" runat="server">
+    Lingzhi's Emotion Studio - Life is Good.
+</asp:Content>
+
+<asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
+
+ 	<ul class="conbox clearfix">
+
+        <%Model.Topics.ForEach(x =>
+          { %>
+            <li><a href="<%:Url.Action("Show","Topic",new{id=x.Slug}) %>" title="<%:x.Title %>"><img width="160" height="100" src="<%:x.Logo %>" alt="" /></a></li>
+        <%}); %>
+    </ul>
+    
+</asp:Content>
