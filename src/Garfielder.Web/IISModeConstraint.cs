d1c0e3cfc9279ae846263a1f255d837e2d5254b8
Levin <Levin@localhost>
2011-02-20 20:51:31 +0800

    ggggg

---
+++ src/Garfielder.Web/IISModeConstraint.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Routing;
+
+namespace Garfielder.Web
+{
+	public class ClassicModeConstraint : IRouteConstraint
+	{
+		public bool Match(HttpContextBase httpContext, Route route, string parameterName, RouteValueDictionary values, RouteDirection routeDirection)
+		{
+			return !HttpRuntime.UsingIntegratedPipeline;
+		}
+	}
+
+	public class IntegratedModeConstraint : IRouteConstraint
+	{
+		public bool Match(HttpContextBase httpContext, Route route, string parameterName, RouteValueDictionary values, RouteDirection routeDirection)
+		{
+			return HttpRuntime.UsingIntegratedPipeline;
+		}
+	}
+
+}
