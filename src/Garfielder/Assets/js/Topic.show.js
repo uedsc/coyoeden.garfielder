ca8e8b9883e1d7a848a7f3e2f5378e615154c2fa
Levin <Levin@localhost>
2011-02-23 00:01:41 +0800

    gggg

---
+++ src/Garfielder/Assets/js/Topic.show.js
@@ -0,0 +1,22 @@
+﻿Garfielder.M("TopicShow", (function () {
+    return {
+        init: function (opts) {
+            this.opts = opts;
+        },
+        onLoad: function () {
+            var $items = $("#gallery .gallery-item");
+            //page tabs
+            $("#pager a").each(function (i, o) {
+                (function (i1, $o) {
+                    $o.click(function () {
+                        if ($o.hasClass("on")) return false;
+                        $o.addClass("on");
+                        return false;
+                    });
+                    $items.hide().eq(i1).fadeIn();
+                })(i, $(o));
+            });
+
+        } //onload
+    };
+} ()));
