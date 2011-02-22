894b83491a5c54fe255b0197fa44cd417e1eecb7
Levin <Levin@localhost>
2011-02-23 00:19:12 +0800

    gggg

--- src/Garfielder/Assets/js/Topic.show.js
+++ src/Garfielder/Assets/js/Topic.show.js
@@ -4,16 +4,22 @@
             this.opts = opts;
         },
         onLoad: function () {
-            var $items = $("#gallery .gallery-item");
+            var $items = $("#gallery .gallery-item"), $curMenu = null;
             //page tabs
             $("#pager a").each(function (i, o) {
                 (function (i1, $o) {
+                    if (i1 == 0) {
+                        $curMenu = $o;
+                    };
                     $o.click(function () {
                         if ($o.hasClass("on")) return false;
-                        $o.addClass("on");
+                        if ($curMenu) {
+                            $curMenu.removeClass("on");
+                        };
+                        $curMenu = $o.addClass("on");
+                        $items.hide().eq(i1).fadeIn();
                         return false;
                     });
-                    $items.hide().eq(i1).fadeIn();
                 })(i, $(o));
             });
 
