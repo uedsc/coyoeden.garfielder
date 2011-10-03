8abb51a40512bc3cebb93220fe2cb0641cf0a81a
Levin <Levin@localhost>
2011-10-03 11:56:06 +0800

    gogo

--- docs/studio/js/data/group.js
+++ docs/studio/js/data/group.js
@@ -1,18 +1,18 @@
 JF.studio.group = [
 	{
-		"id":1,
+		"id":"2266f8c4-65b0-426c-8c76-e85fd6711257",
 		"name":"graphic",
 		"desc":"graphic平面设计"
 	},{
-		"id":2,
+		"id":"43699ad1-06e0-4ca1-a192-c7b9cf90f4c2",
 		"name":"web",
 		"desc":"web网页设计"
 	},{
-		"id":3,
+		"id":"5f15e965-e296-433c-94db-b7326c650a61",
 		"name":"others",
 		"desc":"others"
 	},{
-		"id":4,
+		"id":"98deda39-adf9-4fda-a054-b8c419c7cf36",
 		"name":"shop",
 		"desc":"shop"
 	}
