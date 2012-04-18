92b67cac5fe8470382962c55eab8d96e66cb7e46
levin <levin@localhost>
2012-04-18 23:33:47 +0800

    gogo

---
+++ docs/studio/bak_111217/js/data/tag.js
@@ -0,0 +1,35 @@
+/**
+ * Tags
+ * @author Levin
+ * @version 1.0
+ */
+JF.studio.Tag= [
+	{
+		"id":1,
+		"name":"web design"
+	},{
+		"id":2,
+		"name":"sina"
+	},{
+		"id":3,
+		"name":"sports"
+	},{
+		"id":4,
+		"name":"illustration"
+	},{
+		"id":5,
+		"name":"simple"
+	},{
+		"id":6,
+		"name":"children"
+	},{
+		"id":7,
+		"name":"fasion"
+	},{
+		"id":8,
+		"name":"lookbook"
+	},{
+		"id":9,
+		"name":"style"
+	}
+];
